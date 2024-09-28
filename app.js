07b539781c39951394b7057b2b556bb430ef614e
Imrul Kayes <kayeskayes65@gmail.com>
2024-09-28 22:06:50 +0600

    love calculator

---
+++ app.js
@@ -0,0 +1,11 @@
+let FastName = prompt("What is your name?");
+let PartnerName = prompt("What is your partner name?");
+
+// Calculation
+let number = Math.random() * 100;
+number = Math.floor(number) + 1;
+
+// Showing Result
+alert(
+  FastName + " and " + PartnerName + "'s love percentage is " + number + "%"
+);
